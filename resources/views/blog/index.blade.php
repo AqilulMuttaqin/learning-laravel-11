4689a28d391be94def31d73161179d2580f51c9f
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-09-11 16:32:17 +0700

    model factory seeder

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     @foreach ($blogs as $blog)
         <article class="py-4 border-bottom">
-            <h1>{{ $blog->title }}</h1>
-            <p class="text-secondary">{{ $blog->author }} | {{ $blog->release->translatedFormat('d F Y') }}</p>
+            <a href="{{ route('blog.show', $blog->slug) }}" class="link-underline-dark link-underline-opacity-0 link-underline-opacity-100-hover link-offset-2">
+                <h1 class="text-dark">{{ $blog->title }}</h1>
+            </a>
+            <p class="text-secondary">{{ $blog->author }} | {{ $blog->release->diffForHumans() }}</p>
             <p>{{ Str::limit($blog->body, 200) }}</p>
             <a href="{{ route('blog.show', $blog->slug) }}" class="link-underline link-underline-opacity-0">Read More &raquo;</a>
         </article>
--- resources/views/blog/read-more.blade.php
+++ resources/views/blog/read-more.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <article class="py-4 border-bottom">
-        <h1>{{ $blog['title'] }}</h1>
-        <p class="text-secondary">{{ $blog['author'] }} | {{ \Carbon\Carbon::parse($blog['date'])->translatedFormat('d F Y') }}</p>
-        <p>{{ $blog['body'] }}</p>
-        <a href="{{ route('blog') }}" class="link-underline link-underline-opacity-0">&laquo; Bact to Blog</a>
+        <h1>{{ $blog->title }}</h1>
+        <p class="text-secondary">{{ $blog->author }} | {{ $blog->release->diffForHumans() }}</p>
+        <p>{!! nl2br(e($blog->body)) !!}</p>
+        <a href="{{ route('blog') }}" class="link-underline link-underline-opacity-0">&laquo; Back to Blog</a>
     </article>
 @endsection
